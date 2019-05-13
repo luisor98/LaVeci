6edc0bfc6eede18bc3778df41c1bebfb346ad8b6
Aivils Štoss <aivils@ithouse.lv>
2019-05-13 10:57:00 +0300

    Revert "debug startup.sh"
    This reverts commit 68d1ff2d56695de8756a111b5586f7dad000f70b.

--- script/startup.sh
+++ script/startup.sh
@@ -5,16 +5,13 @@ set -e
 # Expect to be passed either 'web' or 'worker' as parameter
 APP_MODE="${1-web}"
 
-(>&2 echo "startup mode $APP_MODE")
 case "$APP_MODE" in
     web)
-        (>&2 echo "startup reach web")
         if [[ "$MAINTENANCE_MODE" == "true" ]] ; then
             exec /usr/sbin/nginx -p /opt/app -c config/nginx_maintenance.conf
         else
             rm -f tmp/pids/server.pid
 
-            (>&2 echo "startup reach passenger")
             exec bundle exec passenger \
                  start \
                  -p "${PORT-3000}" \
@@ -28,7 +25,6 @@ case "$APP_MODE" in
             # Do nothing
             exec sleep 86400
         else
-            (>&2 echo "startup reach rake jobs:work")
             exec bundle exec rake jobs:work
         fi
         ;;
@@ -37,7 +33,6 @@ case "$APP_MODE" in
             # Do nothing
             exec sleep 86400
         else
-            (>&2 echo "startup reach rake sharetribe:marketplace:run_shredder")
             exec bundle exec rake sharetribe:marketplace:run_shredder["${SHREDDER_SLEEP_TIME-1.5},${SHREDDER_QUERY_SLEEP_TIME-0.2}"]
         fi
         ;;
